edd954e32155d88d041aee6047f8433c0c26ad47
chriskader <chris.kader@gmail.com>
2015-08-07 21:32:56 +0000

    Added everything else!

--- js/app.js
+++ js/app.js
@@ -1,34 +1,56 @@
-$(document).ready(function () {
+$(document).ready(function(){
     $('.ryu').mouseenter(function(){
-        $('.ryu-still').hide();
+        $('.ryu-action').hide();
         $('.ryu-ready').show();
     })
-    .mouseleave(function(){
-        $('.ryu-ready').hide();
+    .mouseleave(function() {
+        $('.ryu-action').hide();
         $('.ryu-still').show();
     })
     .mousedown(function(){
         playHadouken();
-        $('.ryu-ready').hide();
+        $('.ryu-action').hide();
         $('.ryu-throwing').show();
-        $('.hadouken').finish().show()
-          .animate(
+        $('.hadouken').finish().show().animate(
             {'left': '1020px'},
             500,
-            function() {
-              $(this).hide();
-              $(this).css('left', '520px');
-            }
-          );
-        })
-    .mouseup(function(){
-        $('.ryu-throwing').hide();
-        $('.ryu-ready').show();
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
     })
+    .mouseup(function(){
+        $('.ryu-action').hide();
+        $('.ryu-ready').show();
+    });
 });
 
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
+    $(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+        playCool()
+        $('.ryu-action').hide();
+        $('.ryu-cool').show();
+    }   
+    }).keyup(function(e) {
+        if (e.keyCode == 88) {
+        $('#cool-sound')[0].pause();
+        $('#cool-sound')[0].load();
+        $('.ryu-cool').hide();
+        $('.ryu-still').show();
+    }
+    });
+
+function playHadouken() {
+        $('#hadouken-sound')[0].volume = 0.5;
+        $('#hadouken-sound')[0].load();
+        $('#hadouken-sound')[0].play();
+}
+
+var coolSound = false;
+function playCool () {
+  coolSound = !coolSound;
+  if (coolSound) {
+    // $('#cool')[0].load()
+    $('#cool-sound')[0].play();
+  }
 }
